8ad2b7c2d7c66ebdc051a629aa62766ea7c89d66
wahidc7101 <wahidc7101@gmail.com>
2016-07-19 18:19:08 -0400

    initial push

---
+++ JavascriptNotes.js
@@ -0,0 +1,42 @@
+java script notes
+JavaScript primitves
+5 primitve types
+numbers- can be whole numbers, decimals, and negative numbers.
+%modulu returns the remainder of a number.
+strings- are wrapped in quotes
+booleans- are true or fals
+null-
+undefined-
+
+
+the.length returns 2
+count starts at 0
+
+the[2]; returns e
+\\ adds a backsplash to a sentece
+single backslash is an escape key
+use a backslash to escape quotes in strings
+variables are containers that store data
+var name=value;
+
+var name = 'wahid';
+' hello there '+ name;
+
+returns hello there wahid
+varibales can be renamed
+ex:
+var name = 'wahid';
+name ='bob';
+name = bob now.
+variables that are declared but do not have a value it becomes undefined
+var name;
+var age;
+
+null is explicitly nothing
+javascript built in methods
+alert(''); alerts a pop up window
+prompt(''); askes user for input
+console.log(''); displays in the console.
+clear() clears the console
+
+
---
+++ linkingJavascript.js
@@ -0,0 +1,12 @@
+// var userName = prompt("what is your name?");
+// alert("nice to meet you " + userName);
+
+// js stalker exercise
+
+// variables strings prompts and console.log
+// Ask user for full anem, age and print out their answers in a full sentence.
+
+var fullName= prompt(" What is your full name");
+
+var userAge=prompt("how old are you " +  fullName  + " ?")
+alert("Your full name is " + fullName + " you are "+  userAge + 'years old ' + fullName);
