5a8417a290af7d20a46d734b34be87ab4ee24289
wahidc7101 <wahidc7101@gmail.com>
2016-07-25 14:12:17 -0400

    notes

---
+++ forLoops.js
@@ -0,0 +1,59 @@
+// for loops 
+
+// for( intiitalize; condition; step){
+//   // run some code
+// }
+
+// // printing the number 1-5 using a for loop
+
+// var count =1;
+// for(var count = 0; count<6; count++){
+//   console.log(count);
+// }
+
+// // using a while loop to print the numbers 1-5
+
+// var count = 1;
+
+// while(count<6){
+//   console.log("count is: " + count);
+//   count ++;
+// }
+
+// // printing each character in a string using a for loop
+// var str= 'hello';
+// for(var i =0; i <str.length; i++){
+//   console.log(str[i]);
+// }
+// // starts at h since 1 is  and prints every other letter
+// var str = 'ahceclwlxo';
+// for (var i = 1; i < str.length; i+=2){
+//   console.log(str[i]);
+// }
+
+// writing forloops
+// print all numbers between -10 and 19
+
+
+var count =-10;
+for(count =-10; count <=19; count++){
+  console.log(count);
+}
+// print all even numbers between 10 and 40
+
+var i = 10;
+for(i =10; i <=40; i+=2){
+  console.log(i);
+}
+// print all odd numbers between 300 and 333
+var i = 300;
+for(i =300; i<= 333; i+=2){
+  console.log(i);
+}
+
+// print all numbers divisible by 5 and 3 between the number 5 and 50
+
+var i =5;
+for(i =5; i<=50; i){
+  console.log(i);
+}
