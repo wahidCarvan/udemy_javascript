cfe139a047b635deb98fa7c778ba3a129f69e1cb
wahidc7101 <wahidc7101@gmail.com>
2016-07-28 15:08:53 -0400

    added more notes

--- introToArrays.js
+++ introToArrays.js
@@ -1,2 +1,35 @@
 // arrays start with square brackets[]
+// arrays also start at 0 
 var friends =['charlie','liz','david','mattias'];
+// returns charlie
+console.log(friends[0]);
+// returns liz loves david
+friends[1] + " loves  " + friends[2]
+
+// updating array values
+// changes charlie to chuck
+friends[0] = 'chuck';
+// changes david to john
+friends[2] ='john';
+// adding to arrays acces the array 
+var friends =['charlie','liz','david','mattias'];
+// friends displays the whole array
+// adds alex to the array
+friends[4] = "alex";
+// defining  empty arrays arrays
+var friends =[];
+// uncommon way of fdoing it
+var friends = new Array()
+arrays can hold any type of data.
+arrays have a length property
+var nums =[ 45, 37, 89, 24];
+length only counts thr numer of items in an arrays
+nums.length // returns 4
+var name = 'rusty'
+name.length//returns 5 since there are letters in there
+name[4] // returns y because the index starts counting at 4
+array built in methods
+push/propertyshift/unshift
+indexOf
+slice
+
